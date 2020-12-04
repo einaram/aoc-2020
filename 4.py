0c2a0f5c1c0b6450971899b0a9893da401c876ba
Einar Améen <Einar.Ameen@dsa.no>
2020-12-04 17:03:02 +0100

    Day 4 - 2

--- 4.py
+++ 4.py
@@ -1,12 +1,9 @@
 # %%
 import re
-from functools import partial
-
-
 
 def readfile(datatype):
     with open(f"input/4.{datatype}.txt") as infile:
-        passports_batch =infile.read().split("\n\n")
+        passports_batch = infile.read().split("\n\n")
         passports_batch = [x.replace("\n", " ") for x in passports_batch]
         passports = []
         for passport in passports_batch:
@@ -17,15 +14,14 @@ def readfile(datatype):
     return passports
 
 
-
 required_fields = set(["ecl", "pid", "eyr", "hcl",
                        "byr", "iyr", "hgt"])
 
 # A
 def validate_batchA(required_fields, passport_batch):
-    return sum([1 for x in passport_batch if required_fields.issubset(set(x)) ])
-    
-#B
+    return sum([1 for x in passport_batch if required_fields.issubset(set(x))])
+
+# B
 
 def validate_year(ymin, ymax, year):
     valid = False
@@ -54,8 +50,9 @@ def validate_height(height_str):
             valid = True
     return valid
 
+
 def validate_field(field, value):
-    valid= False
+    valid = False
     validators = {
         'byr': lambda year: validate_year(1920, 2002, year),
 
@@ -68,26 +65,23 @@ def validate_field(field, value):
         'hgt': validate_height,
 
         # hcl(Hair Color) - a  # followed by exactly six characters 0-9 or a-f.
-        'hcl': lambda haircolor: re.match("#[0-9a-fA-F]", haircolor),
+        'hcl': lambda haircolor: re.match("#[0-9a-fA-F]{6}", haircolor),
 
         # ecl(Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
         'ecl': lambda x: 1 if x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'] else 0,
 
         # pid(Passport ID) - a nine-digit number, including leading zeroes.
-        'pid': lambda pid: re.match("[0-9]{9}", pid),
+        'pid': lambda pid: re.match("^[0-9]{9}$", pid),
 
         # cid(Country ID) - ignored, missing or not.
         'cid': lambda x: True
     }
     try:
-        valid =validators.get(field)(value)
+        valid = validators.get(field)(value)
     except:
         raise
     finally:
         return valid
-
-
-
 
 def validate_batchB(required_fields, passport_batch):
     valid_count = 0
@@ -100,26 +94,17 @@ def validate_batchB(required_fields, passport_batch):
             valid_field = validate_field(field, value)
             if not valid_field:
                 valid = False
+
         if valid:
-            valid_count +=1
+            valid_count += 1
     print(valid_count)
     return valid_count
-             
 
 assert validate_batchB(required_fields, readfile("test_valid")) == 4
 assert validate_batchB(required_fields, readfile("test_invalid")) == 0
 
-
-validate_batchB(required_fields, readfile("data"))    
-
-
-
+assert validate_batchB(required_fields, readfile("data")) == 179
 
 # A
 assert validate_batchA(required_fields, readfile("test")) == 2
 assert validate_batchA(required_fields, readfile("data")) == 204
-# print("A:", runner(data)) 
-
-
-
-# %%
