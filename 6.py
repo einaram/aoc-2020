a3892fac865c72aecd6bab8cf551bcb9de221d29
Einar Améen <Einar.Ameen@dsa.no>
2020-12-06 10:12:41 +0100

    Cleanup

--- 6.py
+++ 6.py
@@ -1,19 +1,21 @@
 def get_groupansw_fromfile(datatype="input"):
     with open(f"input/6.{datatype}.txt") as infile:
-        answ_batch = [row.replace(" ",'')
-                           for row in infile.read().split("\n\n")]
-    return [answ for answ in answ_batch]
+        return [row.replace(" ", '')
+                for row in infile.read().split("\n\n")]
+
 
 groups_answers = get_groupansw_fromfile()
-#A
+# A
+
 def group_any_count(group_answers):
-    all_ = [len(set(answ.replace("\n", ""))) for answ in group_answers  ]
+    all_ = [len(set(answ.replace("\n", ""))) for answ in group_answers]
     return sum(all_)
+
 
 assert (group_any_count(groups_answers)) == 6625
 
 
-#B
+# B
 
 def group_all_count(group):
     group_answers = [set(x) for x in group.split('\n')]
