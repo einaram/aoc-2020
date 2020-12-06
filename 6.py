a489e37f97a744b50caeafc0870369c055218aed
Einar Améen <Einar.Ameen@dsa.no>
2020-12-06 08:17:39 +0100

    Day 6- 2. Peak list comp.

--- 6.py
+++ 6.py
@@ -1,17 +1,24 @@
 def get_groupansw_fromfile(datatype="input"):
     with open(f"input/6.{datatype}.txt") as infile:
-        answ_batch = [row.replace("\n", " ").replace(" ",'')
+        answ_batch = [row.replace(" ",'')
                            for row in infile.read().split("\n\n")]
     return [answ for answ in answ_batch]
 
-
+groups_answers = get_groupansw_fromfile()
 #A
-all_ = [len(set(answ)) for answ in get_groupansw_fromfile()  ]
-sum_answ= sum(all_)
-assert sum_answ == 6625
+def group_any_count(group_answers):
+    all_ = [len(set(answ.replace("\n", ""))) for answ in group_answers  ]
+    return sum(all_)
+
+assert (group_any_count(groups_answers)) == 6625
+
 
 #B
-for a in get_groupansw_fromfile('test'):
-    pass    
 
+def group_all_count(group):
+    group_answers = [set(x) for x in group.split('\n')]
+    return len(set.intersection(*group_answers))
+
+
+assert sum([group_all_count(group) for group in groups_answers]) == 3360
 # != 1509
