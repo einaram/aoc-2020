18db23e7daa1b89bd86a84f46bb72f585fb3bedc
Einar Améen <Einar.Ameen@dsa.no>
2020-12-06 16:09:44 +0100

    Testing filter Testing list comp. vs filter vs replace.  Filter or replace wins IMO.

--- 6.py
+++ 6.py
@@ -1,18 +1,16 @@
 def get_groupansw_fromfile(datatype="input"):
     with open(f"input/6.{datatype}.txt") as infile:
-        return [row.replace(" ", '')
-                for row in infile.read().split("\n\n")]
+        return infile.read().split("\n\n")
 
-
-groups_answers = get_groupansw_fromfile()
-# A
 
 def group_any_count(group_answers):
-    all_ = [len(set(answ.replace("\n", ""))) for answ in group_answers]
+    #Testing list comp. vs filter vs replace.  Filter or replace wins IMO. 
+    # Maybe split to two for readability
+    # all_replace = [len(set(answ.replace("\n", ""))) for answ in group_answers]
+    # all_lc = [len(set([char for char in answ if char.isalpha()])) for answ in group_answers]
+    all_ = [len(set(filter(str.isalpha, answ))) for answ in group_answers]
+
     return sum(all_)
-
-
-assert (group_any_count(groups_answers)) == 6625
 
 
 # B
@@ -21,6 +19,9 @@ def group_all_count(group):
     group_answers = [set(x) for x in group.split('\n')]
     return len(set.intersection(*group_answers))
 
-
-assert sum([group_all_count(group) for group in groups_answers]) == 3360
-# != 1509
+if __name__ == "__main__":
+    groups_answers = get_groupansw_fromfile()
+    #A
+    assert (group_any_count(groups_answers)) == 6625
+    #B
+    assert sum([group_all_count(group) for group in groups_answers]) == 3360
