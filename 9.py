c26f031d3e1f23180f5f176e967fa8859e04a733
Einar Améen <Einar.Ameen@dsa.no>
2020-12-09 10:44:19 +0100

    Cleanup

--- 9.py
+++ 9.py
@@ -2,11 +2,10 @@ import itertools
 
 def parse_input(datatype="input"):
     with open(f"input/9.{datatype}.txt") as infile:
-        # return [[x.split()[0], int(x.split()[1])] for x in infile.read().split("\n")]
         return [int(x) for x in infile.read().split("\n")]
 
 
-def get_preamble(data, idx, length):
+def preamble_combos(data, idx, length):
     start = idx-length
     return [sum(x) for x in itertools.combinations(data[start:idx], 2) if x[0] != x[1]]
 
@@ -15,7 +14,7 @@ def part1():
     data = parse_input()
     preamble_length = 25
     for r, row in enumerate(data[preamble_length:]):
-        if not row in get_preamble(data, r+preamble_length, preamble_length):
+        if not row in preamble_combos(data, r+preamble_length, preamble_length):
             return row
     
 
@@ -24,7 +23,7 @@ assert part1() == 373803594
 def part2(infile, requested):
     data = parse_input(infile)
     for i in range(2,500):
-        for n, _ in enumerate(data):
+        for n in range(len(data)):
             rolling = data[n:n+i]
             if sum(rolling) == requested:
                 return min(rolling)+max(rolling)
